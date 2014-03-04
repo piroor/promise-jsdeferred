f7e91fe91e62029835a30204c36107ad754dd6a5
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2014-03-05 02:51:05 +0900

    Revert "Extract JS-Code Module specific codes"
    This reverts commit d2bf3b0edf83adde57a2ba273794f8bea7cd8c03.

--- jsdeferred.js
+++ jsdeferred.js
@@ -31,6 +31,25 @@
  * THE SOFTWARE.
  */
 
+var EXPORTED_SYMBOLS = ['Deferred'];
+
+Components.utils.import('resource://gre/modules/Promise.jsm');
+
+if (!setTimeout) {
+  setTimeout = function(aCallback, aInterval) {
+    var timer = Components
+                  .classes['@mozilla.org/timer;1']
+                  .createInstance(Components.interfaces.nsITimer);
+    timer.initWithCallback(aCallback, aInterval, timer.TYPE_ONE_SHOT);
+    timers.push(timer);
+    return timer;
+  };
+
+  clearTimeout = function(aTimer) {
+    aTimer.cancel();
+  };
+}
+
 
 function WrappedPromise(aPromise) {
   this._promise = aPromise;
